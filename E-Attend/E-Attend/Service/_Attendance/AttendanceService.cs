aaf2fc6fdecb4011da7c6eaf5e088d45d17c94f6
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2025-06-09 15:29:35 +0300

    Finalize Attendance service.

---
+++ E-Attend/E-Attend/Service/_Attendance/AttendanceService.cs
@@ -0,0 +1,29 @@
+﻿using E_Attend.Data.Repositories.Implementation;
+using E_Attend.Entities;
+
+namespace E_Attend.Service._Attendance;
+
+public class AttendanceService : IAttendanceService
+{
+    private readonly UnitOfWork _unitOfWork;
+
+    public AttendanceService(UnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public Task<IEnumerable<Attendance>> GetStudentAttendanceInCourseAsync(Course course, Student student)
+    {
+        return _unitOfWork.AttendanceRepository.GetAllAsync(a => a.CourseId == course.Id && a.StudentId == student.Id);
+    }
+
+    public Task<IEnumerable<Attendance>> GetScheduledAttendanceAsync(Course course)
+    {
+        return _unitOfWork.AttendanceRepository.GetAllAsync(a => a.CourseId == course.Id && a.Status == "SCHEDULED");
+    }
+
+    public Task<IEnumerable<Attendance>> GetUnscheduledAttendanceAsync(Course course)
+    {
+        return _unitOfWork.AttendanceRepository.GetAllAsync(a => a.CourseId == course.Id && a.Status == "UNSCHEDULED");
+    }
+}
---
+++ E-Attend/E-Attend/Service/_Attendance/IAttendanceService.cs
@@ -0,0 +1,10 @@
+﻿using E_Attend.Entities;
+
+namespace E_Attend.Service._Attendance;
+
+public interface IAttendanceService
+{
+    public Task<IEnumerable<Attendance>> GetStudentAttendanceInCourseAsync(Course course, Student student);
+    public Task<IEnumerable<Attendance>> GetScheduledAttendanceAsync(Course course);
+    public Task<IEnumerable<Attendance>> GetUnscheduledAttendanceAsync(Course course);
+}
